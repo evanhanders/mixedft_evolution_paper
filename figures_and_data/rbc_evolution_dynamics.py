90fa18426d963373c00f9a5d9041a5031ee6fc09
Evan Anders <evanhanders@gmail.com>
2020-07-09 15:10:07 -0700

    ready to be read by co-authors

--- figures_and_data/rbc_evolution_dynamics.py
+++ figures_and_data/rbc_evolution_dynamics.py
@@ -240,7 +240,7 @@ axtt.set_xlim(0, ffunc(0.5)*2)
 axft.set_xlim(0, mfunc(0.5)*2)
 
 axtt.set_xticks((0, 0.25, 0.5, 0.75))
-axft.set_xticks((0, 0.05, 0.1, 0.15, 0.2, 0.25))
+axft.set_xticks((0, 0.05, 0.1, 0.15, 0.2, 0.25, 0.30))
 axtt.xaxis.set_label_position('top')
 
 
